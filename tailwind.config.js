a55ea9849f65f596a5ce013ff20656ed4531cbaf
HassanZz <hassan.zamani.75@gmail.com>
2022-05-26 11:46:15 +0430

    custom css styles added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./dist/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        mammoth: ["8rem", { lineHeight: "2" }],
+      },
+      colors: {
+        primary: "#FF6363",
+        secondary: {
+          100: "E2E2D5",
+          200: "888883",
+        },
+      },
+    },
   },
   plugins: [],
 };
