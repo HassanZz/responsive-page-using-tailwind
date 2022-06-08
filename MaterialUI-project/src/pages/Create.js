a6a4d7538e253b0ae28c2a7e164ec0d0647086d3
HassanZz <hassan.zamani.75@gmail.com>
2022-06-08 23:42:45 +0430

    Typography added

--- MaterialUI-project/src/pages/Create.js
+++ MaterialUI-project/src/pages/Create.js
@@ -1,5 +1,18 @@
 import React from "react";
+import Typography from "@mui/material/Typography";
 
 export default function Create() {
-  return <div>Create page</div>;
+  return (
+    <div>
+      <Typography
+        variant="h6"
+        component="h2" // we made an h6 tag but in Elemnts of browser you can see h2 tag
+        gutterBottom
+        color="textSecondary"
+        align="center"
+      >
+        New notes
+      </Typography>
+    </div>
+  );
 }
