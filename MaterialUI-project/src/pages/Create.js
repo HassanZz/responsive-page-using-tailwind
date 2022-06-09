c75659842744e4a7d6a11fe28ebc02017d6bb928
HassanZz <hassan.zamani.75@gmail.com>
2022-06-09 23:35:53 +0430

    button added

--- MaterialUI-project/src/pages/Create.js
+++ MaterialUI-project/src/pages/Create.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Typography from "@mui/material/Typography";
+import { Typography, Button, ButtonGroup } from "@mui/material";
 
 export default function Create() {
   return (
-    <div>
+    <>
       <Typography
         variant="h6"
         component="h2" // we made an h6 tag but in Elemnts of browser you can see h2 tag
@@ -13,6 +13,24 @@ export default function Create() {
       >
         New notes
       </Typography>
-    </div>
+      <Button
+        onClick={() => console.log("you clicked me")}
+        type="submit"
+        color="secondary"
+        variant="contained"
+
+        // disableElevation
+      >
+        Submit
+      </Button>
+      {/* <Button type="submit" color="secondary" variant="outlined">
+        Submit
+      </Button>
+      <ButtonGroup color="secondary" variant="contained">
+        <Button>One</Button>
+        <Button>Two</Button>
+        <Button>Three</Button>
+      </ButtonGroup> */}
+    </>
   );
 }
